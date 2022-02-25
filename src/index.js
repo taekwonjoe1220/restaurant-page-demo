84662ce085e4f430bc62f3c40952b32eeab4a38c
Joe Forster <jforster.advanced@gmail.com>
2022-02-25 11:21:04 -0700

    updated for css integration, made navbar template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("TESTING");
+// Imports
+import './style.css';
+    // Home, Products, Coffee, About, Contact
+// House main layout?
+
+// Nav Bar
+
+// Content placeholder (dynamic loading here)
+
+// Footer
+
+// House logic for loading content dynamically with event listeners
+
+// DOM caching
+const content = document.getElementById('content');
+
+// render
