6b97eb473db83d080044ed22a9c8e2d384740ce1
Joe Forster <jforster.advanced@gmail.com>
2022-02-27 08:39:46 -0700

    added bg imgs to img src folder and set background on home page

--- src/home.js
+++ src/home.js
@@ -1,13 +1,14 @@
 "use strict";
 
+// DOM caching for content background
+const content = document.getElementById("content");
+
 // Home Page body content ** In Progress **
 function createHome() {
   const home = document.createElement("div");
-  home.classList.add("home");
+  home.classList.add("home-content");
 
   // testing with some lorem text
-  home.textContent =
-    "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus dolore ad tenetur ratione sed assumenda excepturi, accusantium error consequuntur nihil soluta tempore ipsam ipsum rem aliquid impedit alias blanditiis nisi libero? Assumenda optio ad ratione minima ex porro temporibus dolorum doloribus natus voluptatum ipsam, est nulla sed. Dolor, excepturi a";
   return home;
 }
 
